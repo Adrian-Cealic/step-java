07ae0a2d4a19bbf8e46dfd7f8ddba7dba058c780
Adrian-Cealic <adrian.cealic@gmail.com>
2024-02-13 19:46:45 +0200

    added first atestation

---
+++ math/App.java
@@ -0,0 +1,10 @@
+package com.step.testare.edu.step.math;
+import com.step.testare.edu.step.math.Calculator;
+public class App {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        //calc.tablaInmultirii(5);
+
+        //calc.sumaNumerelor(5);
+    }
+}
